e4e7ed23038dd6f3cd842e35906cad38f0900eb9
Lane Harrison <lanetharrison@gmail.com>
2014-08-03 23:46:53 +0000

    New preview logo

--- js/pubs.js
+++ js/pubs.js
@@ -93,7 +93,7 @@ function buildPreviews() {
     //.attr('src', function(d) { return 'img/' + d.key + '-preview.png'; });
     .attr('src', function(d) { 
       if(d.img) return d.img;
-      else return 'img/logo-2.png'; 
+      else return 'img/preview/preview-logo.png'; 
     });
 }
 
